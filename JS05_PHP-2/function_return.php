673ff4e78054a668dcf5173ee0d2e3f17201e16f
Yosep Bima Aprillian <byosep050@gmail.com>
2025-09-29 19:05:36 +0700

    Jobsheet 5 Prak 4 Fungsi dgn nilai pengembalian

---
+++ JS05_PHP-2/function_return.php
@@ -0,0 +1,9 @@
+<?php
+function hitungUmur($thn_lahir, $thn_sekarang){
+    $umur = $thn_sekarang - $thn_lahir;
+    return $umur;
+}
+
+echo "Umur saya adalah ". hitungUmur(2005, 2025) ." tahun";
+
+?>
