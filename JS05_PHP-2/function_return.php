c55a39541bca020627e9d92f66c1f760644a7bd6
Yosep Bima Aprillian <byosep050@gmail.com>
2025-09-29 19:20:19 +0700

    Jobsheet 5 Prak 4 Fungsi di dalam fungsi lain

--- JS05_PHP-2/function_return.php
+++ JS05_PHP-2/function_return.php
@@ -4,6 +4,13 @@ function hitungUmur($thn_lahir, $thn_sekarang){
     return $umur;
 }
 
-echo "Umur saya adalah ". hitungUmur(2005, 2025) ." tahun";
+function perkenalan($nama, $salam="Assalamualaikum"){
+    echo $salam.",";
+    echo "Perkenalkan, nama saya ".$nama."<br/>";
 
+    echo "Saya berusia ". hitungUmur(1988, 2023) ." tahun<br/>";
+    echo "Senang berkenalan dengan Anda<br/>";
+}
+
+perkenalan("Elok");
 ?>
