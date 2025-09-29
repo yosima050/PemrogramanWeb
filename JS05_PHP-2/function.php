90c5d0804622be41cb4991d4dc5c5ceae43fc2df
Yosep Bima Aprillian <byosep050@gmail.com>
2025-09-29 18:54:49 +0700

    Jobsheet 5 Prak 4 Fungsi dgn Parameter & Menggunakan nilai default

--- JS05_PHP-2/function.php
+++ JS05_PHP-2/function.php
@@ -1,5 +1,5 @@
 <?php
-function perkenalan($nama, $salam){
+function perkenalan($nama, $salam="Assalamualaikum"){
     echo $salam.", ";
     echo "Perkenalkan, nama saya ".$nama."<br/>";
     echo "Senang berkenalan dengan Anda<br/>";
@@ -11,5 +11,6 @@ echo "<hr>";
 
 $saya = "Elok";
 $ucapanSalam = "Selamat pagi";
-perkenalan($saya, $ucapanSalam);
+
+perkenalan($saya);
 ?>
