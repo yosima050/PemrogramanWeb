5ae746ca16ee3814005bec89a64d5e9cc7bf7695
Yosep Bima Aprillian <byosep050@gmail.com>
2025-09-29 17:16:28 +0700

    Jobsheet 5 Prak 4 Fungsi

---
+++ JS05_PHP-2/function.php
@@ -0,0 +1,11 @@
+<?php
+function perkenalan(){
+    echo "Assalamualaikum, ";
+    echo "Perkenalkan, nama saya Elok<br/>";
+    echo "Senang berkenalan dengan Anda<br/>";
+}
+
+perkenalan();
+echo "<br>";
+perkenalan();
+?>
