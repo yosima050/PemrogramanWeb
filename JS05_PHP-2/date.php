e3501a7a99ec486254ac7b0aa2066c44706d4da4
Yosep Bima Aprillian <byosep050@gmail.com>
2025-10-01 23:58:29 +0700

    Jobsheet 5 Prak 10

---
+++ JS05_PHP-2/date.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Date</title>
+</head>
+<body>
+    <h3>Date</h3>
+    <?php
+        echo "Today is " . date("Y/m/d") . "<br>";
+        echo "Today is " . date("Y.m.d") . "<br>";
+        echo "Today is " . date("Y-m-d") . "<br>";
+        echo "Today is " . date("l");
+    ?>
+</body>
+</html>
---
+++ JS05_PHP-2/time.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Time</title>
+</head>
+<body>
+    <h3>Time</h3>
+    <?php
+        date_default_timezone_set("Asia/Jakarta");
+        echo date("h:i:sa");
+    ?>
+</body>
+</html>
