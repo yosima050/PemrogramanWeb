69fefd842fde43d177ae91f7c4943e93b6792950
Yosep Bima Aprillian <byosep050@gmail.com>
2025-10-12 14:47:05 +0700

    Jobsheet 7 Prak 1

--- dasarWeb/isset.php
+++ dasarWeb/isset.php
@@ -1,8 +1,16 @@
 <?php
+
 $umur;
 if (isset($umur) && $umur >= 18) {
-    echo "Anda sudah dewasa.";
+    echo "Anda sudah dewasa." . "<br>";
 } else {
-    echo "Anda belum dewasa atau variabel 'umur' tidak ditemukan.";
+    echo "Anda belum dewasa atau variabel 'umur' tidak ditemukan." . "<br>";
+}
+
+$data = array("nama" => "Jane", "usia" => 25);
+if (isset($data["nama"])) {
+    echo "Nama: " . $data["nama"];
+} else {
+    echo "Variabel 'nama' tidak ditemukan dalam array.";
 }
 ?>
