5cbbc4db72f0079459d3bcb38a706e3a9e411560
Yosep Bima Aprillian <byosep050@gmail.com>
2025-10-28 09:54:12 +0700

    Jobsheet Prak 1 | 9

--- dasarWeb/Jobsheet-8/upload.php
+++ dasarWeb/Jobsheet-8/upload.php
@@ -1,7 +1,8 @@
 <?php
 if(isset($_POST["submit"])){
     $targetdir = "uploads/";
-    $targetfile = $targetdir . basename($_FILES["myfile"]["name"]);
+    $filename = basename($_FILES["myfile"]["name"]);
+    $targetfile = $targetdir . $filename;
     $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
     
     $allowedExtensions = array("jpg", "jpeg", "png", "gif");
@@ -10,6 +11,14 @@ if(isset($_POST["submit"])){
     if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"] <= $maxsize) {
         if(move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)){
             echo "File berhasil diunggah.";
+
+            $urlSafeName = rawurlencode($filename);
+            $imgSrc = $targetdir . $urlSafeName;
+            $alt = htmlspecialchars($filename, ENT_QUOTES);
+            echo "<div style=\"margin-top:10px;\">";
+            echo "<p>Thumbnail:</p>";
+            echo "<img src=\"{$imgSrc}\" alt=\"{$alt}\" width=\"200\" style=\"height:auto; border:1px solid #ccc;\" >";
+            echo "</div>";
         }
         else{
             echo "Gagal mengunggah file.";
