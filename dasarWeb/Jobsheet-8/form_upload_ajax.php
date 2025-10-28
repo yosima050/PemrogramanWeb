42db378cd6e1c7c7ee7699eceb7a64b090d0c541
Yosep Bima Aprillian <byosep050@gmail.com>
2025-10-28 20:55:16 +0700

    Jobsheet 8 Prak 4

--- dasarWeb/Jobsheet-8/form_upload_ajax.php
+++ dasarWeb/Jobsheet-8/form_upload_ajax.php
@@ -1,42 +1,26 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <link rel="stylesheet" type="text/css" href="upload.css">
     <title>Unggah File Dokumen</title>
 </head>
 <body>
-
-    <form id="upload-form-gambar" action="upload_gambar_ajax.php" method="post" enctype="multipart/form-data">
-        <input type="file" name="file[]" id="file" multiple accept="image/png, image/jpeg, image/gif">
-        <input type="submit" name="submit" value="Unggah Gambar">
-    </form>
-
-    <div id="status"></div>
+    <div class="upload-form-container">
+        <h2>Unggah File Dokumen</h2>
+        
+        <form id="upload-form" action="upload.php" method="post" enctype="multipart/form-data">
+            <div class="file-input-container">
+                <input type="file" name="file" id="file" class="file-input">
+                <label for="file" class="file-label">Pilih File</label>
+            </div>
+            
+            <button type="submit" name="submit" class="upload-button" id="upload-button" disabled>Unggah</button>
+        </form>
+        
+        <div id="status" class="upload-status"></div>
+    </div>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    
-    <script>
-    $(document).ready(function(){
-        $('#upload-form').submit(function(e){
-            e.preventDefault(); 
-            
-            var formData = new FormData(this);
-
-            $.ajax({
-                type: 'POST',
-                url: 'upload_ajax.php',
-                data: formData,
-                cache: false,
-                contentType: false,
-                processData: false,
-                success: function(response){
-                    $('#status').html(response);
-                },
-                error: function(){
-                    $('#status').html('Terjadi kesalahan saat mengunggah file.');
-                }
-            });
-        });
-    });
-    </script>
+    <script src="upload.js"></script>
 </body>
 </html>
