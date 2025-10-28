68c16d4bd6575f9c5726a911d7060bdf03542e59
Yosep Bima Aprillian <byosep050@gmail.com>
2025-10-28 09:23:23 +0700

    Jobsheet Prak 1 | 8

--- dasarWeb/Jobsheet-8/upload.php
+++ dasarWeb/Jobsheet-8/upload.php
@@ -2,12 +2,21 @@
 if(isset($_POST["submit"])){
     $targetdir = "uploads/";
     $targetfile = $targetdir . basename($_FILES["myfile"]["name"]);
+    $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
+    
+    $allowedExtensions = array("jpg", "jpeg", "png", "gif");
+    $maxsize = 5 * 1024 * 1024;
 
-    if(move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)){
-        echo "File berhasil diunggah.";
+    if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"] <= $maxsize) {
+        if(move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)){
+            echo "File berhasil diunggah.";
+        }
+        else{
+            echo "Gagal mengunggah file.";
+        }
     }
-    else{
-        echo "Gagal mengunggah file.";
+    else {
+        echo "File tidak valid atau melebihi ukuran maksimum yang diizinkan";
     }
 }
 ?>
