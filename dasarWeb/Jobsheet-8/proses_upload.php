4c12c2941323751761ee472afd4baf9ab7e4a0ba
Yosep Bima Aprillian <byosep050@gmail.com>
2025-10-28 19:44:40 +0700

    Jobsheet 8 Prak 2

---
+++ dasarWeb/Jobsheet-8/proses_upload.php
@@ -0,0 +1,24 @@
+<?php
+$targetDirectory = "documents/";
+
+if (!file_exists($targetDirectory)) {
+    mkdir($targetDirectory, 0777, true);
+}
+
+if (isset($_FILES['files']['name'][0]) && !empty($_FILES['files']['name'][0])) {
+    $totalFiles = count($_FILES['files']['name']);
+
+    for ($i = 0; $i < $totalFiles; $i++) {
+        $fileName = $_FILES['files']['name'][$i];
+        $targetFile = $targetDirectory . $fileName;
+
+        if (move_uploaded_file($_FILES['files']['tmp_name'][$i], $targetFile)) {
+            echo "File $fileName berhasil diunggah.<br>";
+        } else {
+            echo "Gagal mengunggah file $fileName.<br>";
+        }
+    }
+} else {
+    echo "Tidak ada file yang diunggah.";
+}
+?>
